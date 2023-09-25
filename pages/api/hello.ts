e5f1619a88a885fb0b6b99b677f6245186eabf67
Haruki Fukuda <hf6571@gmail.com>
2023-09-25 14:11:49 +0900

    apitest1

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -9,5 +9,10 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
+  // CORS設定: すべてのオリジンからのアクセスを許可
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  
   res.status(200).json({ name: 'John Doe' })
 }
